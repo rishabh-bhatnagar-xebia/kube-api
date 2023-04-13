968c7d8d588085e59d351190aac7ce8581814c62
rishabh-bhatnagar-xebia <rishabh.bhatnagar@xebia.com>
2023-04-13 20:10:03 +0530

    Fill resolvePort Function
    Signed-off-by: rishabh-bhatnagar-xebia <rishabh.bhatnagar@xebia.com>

--- utils.go
+++ utils.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func resolvePort() string {
-	return PORT
+	port := PORT
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+		log.Println("got an inline value for the port:", port)
+	}
+	return port
 }
